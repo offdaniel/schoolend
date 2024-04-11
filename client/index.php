926cba423ea921ff4444b6fcc7ef066073209853
SDaniel <144014826+offdaniel@users.noreply.github.com>
2024-04-11 10:03:05 +0200

    Add files via upload

--- client/index.php
+++ client/index.php
@@ -37,7 +37,7 @@
         <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
             <ul class="navbar-nav fs-5">
                 <li class="nav-item" style="margin-right: 10px;">
-                    <a class="nav-link" href="index.php">Fő oldal</a>
+                    <a class="nav-link" href="index.php">Főoldal</a>
                 </li>
                 <li class="nav-item" style="margin-right: 10px;">
                     <a class="nav-link" href="index.php?prog=benzines.php">Benzines gépek</a>
